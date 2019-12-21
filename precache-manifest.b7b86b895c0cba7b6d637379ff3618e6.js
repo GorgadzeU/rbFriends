6f9fb298e58302631753701251eb12a67eec24c9
Your Name <you@example.com>
2019-12-21 17:51:24 +0400

    Updates

---
+++ precache-manifest.b7b86b895c0cba7b6d637379ff3618e6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a8952e359988a550b0c26c9c616fc3f",
+    "url": "/rbFriends/index.html"
+  },
+  {
+    "revision": "d36d9148cc4f5df99e00",
+    "url": "/rbFriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "d6819555c3f435e0b1d4",
+    "url": "/rbFriends/static/css/main.2dd60e34.chunk.css"
+  },
+  {
+    "revision": "d36d9148cc4f5df99e00",
+    "url": "/rbFriends/static/js/2.4174fdd6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/rbFriends/static/js/2.4174fdd6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d6819555c3f435e0b1d4",
+    "url": "/rbFriends/static/js/main.6a70f0c7.chunk.js"
+  },
+  {
+    "revision": "cbf28ceea7398f4ac904",
+    "url": "/rbFriends/static/js/runtime-main.b14c9bc2.js"
+  }
+]);
